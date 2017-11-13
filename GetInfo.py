92b4a8120c2696d84fe66c54b94c1d9c64313d73
Xaris <kayaku@protonmail.com>
2017-11-13 16:11:34 +0200

    start coding reverse domain check
    Reverse IP Domain Check

--- GetInfo.py
+++ GetInfo.py
@@ -1,6 +1,7 @@
 import os
 from socket import *
 import dns.resolver
+import dns.reversename
 
 # TODO Implement a module that gets information about the host
 # TODO Pass arguments from terminal
@@ -13,6 +14,7 @@ def reach_host(hostname,arguments='-c 1'):
         print "Host appears to be up "
     else:
         print "Host is down or does not reply to ping requests "
+    print "Host's ip is " + gethostbyname(hostname)
 
 
 def nslookup(hostname, typ3='MX'):
@@ -20,6 +22,10 @@ def nslookup(hostname, typ3='MX'):
     for rdata in answers:
         print 'Host', rdata.exchange, 'has preference', rdata.preference
 
+def name_reverse(hostip):
+    n = dns.reversename.from_address(hostip)
+    print(n)
+    #print(dns.reversename.to_address(n))
 
 def main():
     hostname = "dnspython.org"
@@ -27,6 +33,8 @@ def main():
     reach_host(hostname)
     print "Mail server lookup "
     nslookup(hostname)
+    print '[+] Reverse ip domain checking '
+    # TODO Reverse ip domain check
 
 
 if __name__ == "__main__":
